e4e6acfe368d585d2d7b50456fd10ea0259266da
Adrian <adrianhelvik100@gmail.com>
2016-06-22 09:58:25 +0200

    ...

---
+++ index.js
@@ -0,0 +1,341 @@
+module.exports = {
+    deleteAll,
+    getAllMethods,
+    getDefaultParams,
+    getMethodsWithPrefix,
+    lowerCaseFirst,
+    delimiterParser,
+    removePrefix,
+    throwIfDefined,
+
+    getRawParameters,
+    splitRawParams,
+    hasMethods,
+    implementOrThrow,
+    isDefined,
+    removeSuffix,
+    upperCaseFirst,
+};
+
+function deleteAll(obj, attrs) {
+    attrs.forEach(attr => delete obj[attr]);
+}
+
+function throwIfDefined(value, msg) {
+    if (value !== undefined) {
+        throw TypeError(msg);
+    }
+}
+
+function removePrefix(original, prefix) {
+    return original.substring(prefix.length, original.length);
+}
+
+function removeSuffix(original, suffix) {
+    return original.substring(0, original.length - suffix.length);
+}
+
+function lowerCaseFirst(string) {
+    return string[0].toLowerCase() + string.substring(1, string.length);
+}
+
+function upperCaseFirst(string) {
+    return string[0].toUpperCase() + string.substring(1, string.length);
+}
+
+function delimiterParser(someJavascript, conditions, postCondition) {
+    const string = someJavascript.toString();
+
+    if (! Array.isArray(conditions)) {
+        conditions = [conditions];
+    }
+
+    const balance = {
+        allZero() {
+            return this.paren === 0 && this.curly === 0 && this.brack === 0;
+        },
+        paren: 0,
+        curly: 0,
+        brack: 0,
+        comments: {
+            single: 0,
+            multi: 0
+        }
+    };
+
+    for (let i = 0; i < string.length; i++) {
+
+        const pairBefore = function (start, end) {
+            if (! Array.isArray(start)) {
+                start = [start];
+            }
+            if (! Array.isArray(end)) {
+                end = [end];
+            }
+
+            const matches = start.filter(token => token === string.substring(i, i + token.length));
+
+            if (matches.length) {
+                i += matches[0].length - 1;
+
+                return 1;
+            }
+
+            return 0;
+        };
+
+        const pairAfter = function (start, end) {
+            if (! Array.isArray(start)) {
+                start = [start];
+            }
+            if (! Array.isArray(end)) {
+                end = [end];
+            }
+
+            const matches = end.filter(token => token === string.substring(i, i + token.length));
+
+            if (matches.length) {
+                i += matches[0].length - 1;
+                return -1;
+            }
+
+            return 0;
+        };
+
+        const trap = function (start, end) {
+            if (! Array.isArray(start)) {
+                start = [start];
+            }
+            if (! Array.isArray(end)) {
+                end = [end];
+            }
+
+            for (const startToken of start) {
+                if (startToken === string.substring(i, i + startToken.length)) {
+                    i += startToken.length;
+
+                    while (true) {
+                        if (i >= string.length) {
+                            throw Error('Trap out of bounds');
+                        }
+                        for (const endToken of end) {
+                            if (endToken === string.substring(i, i + endToken.length)) {
+                                i += endToken.length - 1;
+                                return true;
+                            }
+                        }
+                        i++;
+                    }
+                }
+            }
+        };
+
+        const isNext = function (str) {
+            return string.substring(i, i + str.length) === str;
+        };
+
+        const letter = string[i];
+
+        // pre condition parsing
+        balance.paren += pairBefore('(', ')');
+        balance.curly += pairBefore('{', '}');
+        balance.brack += pairBefore('[', ']');
+
+        if (trap('/*', '*/')) {
+            continue;
+        }
+        if (trap('//', ['\n', '\r'])) {
+            continue;
+        }
+
+        // call all conditions unless in comment
+        if (! balance.comments.single && ! balance.comments.multi) {
+            let inc;
+
+            conditions.some(condition => {
+                inc = condition(balance, string, i, isNext);
+
+                if (typeof inc === 'number') {
+                    i += inc;
+                    return true;
+                }
+            });
+        }
+
+        // post condition parsing
+        balance.paren += pairAfter('(', ')');
+        balance.curly += pairAfter('{', '}');
+        balance.brack += pairAfter('[', ']');
+    }
+
+    postCondition && postCondition(balance, string);
+
+}
+
+function getAllMethods(clazz) {
+    const methodNames = [];
+    let raw = [];
+
+    delimiterParser(clazz.toString(), [
+        (balance, string, i) => {
+            if (balance.curly === 1 && string[i].trim() && balance.paren === 0 && ! ['{','}'].includes(string[i])) {
+                raw.push(string[i]);
+            } else if (raw.length) {
+                methodNames.push(raw.join(''));
+                raw = [];
+            }
+        }
+    ]);
+
+    return methodNames;
+}
+
+function getMethodsWithPrefix(clazz, prefix) {
+    return getAllMethods(clazz)
+        .filter(methodName => methodName.startsWith(prefix));
+}
+
+function getDefaultParams(fn) {
+    const splitRaw = splitRawParams(getRawParameters(fn));
+
+    const result = {};
+
+    for (const param of splitRaw) {
+        let isDone = false;
+
+        delimiterParser(param, [
+            (balance, string, i, isNext) => {
+                if (isDone) { return; }
+
+                const isAssignment = string[i] === '=';
+
+                if (balance.allZero() && string[i] === '=') {
+                    const key = string.substring(0, i).trim();
+                    const val = string.substring(i+1, string.length).trim();
+
+                    result[key] = val;
+
+                    isDone = true;
+                }
+            }
+        ]);
+
+        if (! isDone) {
+            result[param] = null;
+        }
+    }
+
+    return result;
+}
+
+function splitRawParams(raw) {
+
+    let current = [];
+    let delim;
+    const result = [];
+
+    delimiterParser(raw, [
+        (balance, string, i, isNext) => {
+
+            // case: strings
+            if (string[i] === '\'' || string[i] === '"') {
+                delim = string[i];
+                current.push(string[i]);
+                while (true) {
+                    i++;
+                    current.push(string[i]);
+                    if (string[i] === '\\') {
+                        i++;
+                        current.push(string[i]);
+                        continue;
+                    }
+                    if (string[i] === delim) {
+                        break;
+                    }
+                }
+                return i;
+            }
+
+            // case split
+            else if (string[i] === ',' && balance.allZero()) {
+                result.push(current.join('').trim());
+                current = [];
+            }
+
+            else {
+                current.push(string[i]);
+            }
+        }
+    ]);
+
+    if (current.length) {
+        result.push(current.join('').trim());
+    }
+
+    return result;
+}
+
+function getRawParameters(fn) {
+    let raw = [];
+    let isDone = false;
+    let stripParen = false;
+
+    delimiterParser(fn.toString(), [
+        (balance, string, i, isNext) => {
+            if (isDone) { return; }
+
+            // case: () arguments
+            if (balance.paren >= 1) {
+                raw.push(string[i]);
+                stripParen = true;
+                return;
+            }
+
+            // case (param)=>...
+            if (raw.length && balance.paren === 0) {
+                isDone = true;
+                return;
+            }
+
+            // case: arg => {}
+            if (balance.curly === 0 && isNext('=>')) {
+                isDone = true;
+                raw.push(string.substring(0, i));
+                return;
+            }
+
+            /*
+               if (balance.curly === 0 && balance.paren === 1 && ! ['(',')'].includes()) {
+               console.log()
+               }
+               */
+        }
+    ]);
+
+    raw = raw.join('');
+
+    if (stripParen) {
+        raw = raw.substring(1, raw.length - 1);
+    }
+
+    return raw;
+}
+
+function hasMethods(instance, methods) {
+    for (const method of methods) {
+        if (typeof instance[method] !== 'function') {
+            return false;
+        }
+    }
+    return true;
+}
+
+function implementOrThrow(instance, methods) {
+    if (! hasMethods(instance, methods)) {
+        throw TypeError('Object does not implement methods: ' + JSON.stringify(methods));
+    }
+}
+
+function isDefined(val) {
+    return val !== undefined;
+}
---
+++ test/util-spec.js
@@ -0,0 +1,328 @@
+const util = require('../index');
+const assert = require('assert');
+
+describe('util', () => {
+    describe('.deleteAll', () => {
+        it('deletes all specified props from an object', () => {
+            const obj = { a: 1, b: 2, c: 3 };
+
+            util.deleteAll(obj, ['a', 'c']);
+
+            assert.equal('a' in obj, false);
+            assert.equal('c' in obj, false);
+            assert.equal('b' in obj, true);
+        });
+    });
+
+    describe('.getAllMethods', () => {
+        it('gets all method names from a class', () => {
+            class MyClass {
+                a/*hello*/() {}
+                b//world
+                () {}
+            }
+
+            assert.deepEqual(util.getAllMethods(MyClass), ['a', 'b']);
+        });
+    });
+
+    describe('.getDefaultParams', () => {
+        it('parses default params', () => {
+            function fn(a = b, c = d + e, f = "g, h\" = i") {} // eslint-disable-line
+
+            const params = util.getDefaultParams(fn);
+
+            assert.equal(params.a, 'b');
+            assert.equal(params.c, 'd + e');
+            assert.equal(params.f, '"g, h\\" = i"');
+        });
+
+        it('doesn\'t touch params in an object. Eg: fn({ a = 10, b = 20 })', () => {
+            function fn({ a = 10, b = 20 } = {}) {} // eslint-disable-line
+
+            const params = util.getDefaultParams(fn);
+
+            assert.deepEqual(params['{ a = 10, b = 20 }'], '{}');
+        });
+
+        it('is stored as a key with an undefined value if no default param was given', () => {
+            function fn(a, b) {}
+
+            const params = util.getDefaultParams(fn);
+
+            assert.deepEqual(params, { 'a': null, 'b': null });
+        });
+    });
+
+    describe('.splitRawParams', () => {
+        it('splits raw params', () => {
+            function fn(a = b, c = d + e, f = "g, h\" = 'i'") {} // eslint-disable-line
+
+            const rawParams = util.getRawParameters(fn);
+            const params = util.splitRawParams(rawParams);
+
+            assert.deepEqual(params, ['a = b', 'c = d + e', 'f = "g, h\\" = \'i\'"']);
+        });
+
+        it('doesn\'t touch params in an object. Eg: fn({ a, b })', () => {
+            function fn({ a = 10, b = 20 } = {}) {} // eslint-disable-line
+
+            const rawParams = util.getRawParameters(fn);
+            const params = util.splitRawParams(rawParams);
+
+            assert.deepEqual(params, ['{ a = 10, b = 20 } = {}']);
+        });
+
+        it('doesn\'t touch params in an array. Eg: fn([ a, b ])', () => {
+            function fn([ a = 10, b = 20 ]) {} // eslint-disable-line
+
+            const rawParams = util.getRawParameters(fn);
+            const params = util.splitRawParams(rawParams);
+
+            assert.deepEqual(params, ['[ a = 10, b = 20 ]']);
+        });
+    });
+
+    describe('.getRawParameters', () => {
+
+        it('works for regular functions', () => {
+            function fn(a = 10, b) { }
+            const params = util.getRawParameters(fn);
+            assert.equal(params, 'a = 10, b');
+        });
+
+        it('works for unnamed functions', () => {
+            const params = util.getRawParameters(function(a=10,b){});
+            assert.equal(params, 'a=10,b');
+        });
+
+        it('works for class method', () => {
+            class MyClass {
+                fn (a = (10), b) {
+                }
+            }
+
+            const instance = new MyClass;
+
+            const paramsA = util.getRawParameters(MyClass.prototype.fn);
+            const paramsB = util.getRawParameters(MyClass.prototype.fn);
+
+            assert.equal(paramsA, paramsB);
+            assert.equal(paramsA, 'a = (10), b');
+        });
+
+        it('works for arrow functions w/o parens and with curlies', () => {
+            const fn=param=>{}; // eslint-disable-line
+            const params = util.getRawParameters(fn);
+            assert.equal(params, 'param');
+        });
+
+        it('works for arrow functions w/o parens and w/o curlies', () => {
+            const fn=param=>param; // eslint-disable-line
+            const params = util.getRawParameters(fn);
+            assert.equal(params, 'param');
+        });
+
+        it('works for arrow functions with parens and with curlies', () => {
+            const fn=(param)=>{param}; // eslint-disable-line
+            const params = util.getRawParameters(fn);
+            assert.equal(params, 'param');
+        });
+
+        it('works for arrow functions with parens and w/o curlies', () => {
+            const fn=(param, b=(x+2))=>param; // eslint-disable-line
+            const params = util.getRawParameters(fn);
+            assert.equal(params, 'param, b=(x+2)');
+        });
+
+        it('works for arrow functions with parens no arguments', () => {
+            const fn=()=>{}; // eslint-disable-line
+            const params = util.getRawParameters(fn);
+            assert.equal(params, '');
+        });
+
+        it('works for terse methods with params', () => {
+            const obj = {
+                myFunc(a = 1, c = (2 + 3)) {
+                }
+            };
+            const params = util.getRawParameters(obj.myFunc);
+            assert.equal(params, 'a = 1, c = (2 + 3)');
+        });
+        it('works for terse methods w/o params', () => {
+            const obj = {
+                myFunc() {
+                }
+            };
+            const params = util.getRawParameters(obj.myFunc);
+            assert.equal(params, '');
+        });
+    });
+
+    describe('.getMethodsWithPrefix', () => {
+        it('gets the method names that has the given prefix from a class', () => {
+            class MyClass {
+                preHello() {}
+                lol() {}
+            }
+
+            assert.deepEqual(util.getMethodsWithPrefix(MyClass, 'pre'), ['preHello']);
+        });
+    });
+
+    describe('.lowerCaseFirst', () => {
+        it('converts the first letter of a string to lowercase', () => {
+            assert.equal(util.lowerCaseFirst('Hello'), 'hello');
+            assert.equal(util.lowerCaseFirst('HELLO'), 'hELLO');
+            assert.equal(util.lowerCaseFirst('hello'), 'hello');
+        });
+    });
+
+    describe('.delimiterParser', () => {
+        it('keeps track of parens', () => {
+            let finalBalance;
+            let greatest = 0;
+
+            util.delimiterParser('( )', (balance, string, i) => {
+                if (balance.paren > greatest) {
+                    greatest = balance.paren;
+                }
+            }, balance => {
+                finalBalance = balance;
+            });
+
+            assert.equal(finalBalance.paren, 0);
+            assert.equal(greatest, 1);
+        });
+
+        it('keeps track of curlies', () => {
+            let finalBalance;
+            let greatest = 0;
+
+            util.delimiterParser('{ }', (balance) => {
+                if (balance.curly > greatest) {
+                    greatest = balance.curly;
+                }
+            }, balance => {
+                finalBalance = balance;
+            });
+
+            assert.equal(finalBalance.curly, 0);
+            assert.equal(greatest, 1);
+        });
+
+        it('keeps track of brackets', () => {
+            let finalBalance;
+            let greatest = 0;
+
+            util.delimiterParser('[ ]', (balance) => {
+                if (balance.brack > greatest) {
+                    greatest = balance.brack;
+                }
+            }, balance => {
+                finalBalance = balance;
+            });
+
+            assert.equal(finalBalance.brack, 0);
+            assert.equal(greatest, 1);
+        });
+
+        it('ignores multi line comments', () => {
+            const str = 'a/*b//*/c/*  */';
+            const expected = 'ac';
+            let res = '';
+
+            util.delimiterParser(str, (balance, str, i) => {
+                res += str[i];
+            });
+
+            assert.equal(res, expected);
+        });
+
+        it('ignores single line comments', () => {
+            const str = `a///*b
+            c`;
+            const expected = 'ac';
+            let res = '';
+
+            util.delimiterParser(str, (balance, str, i) => {
+                res += str[i].trim();
+            });
+
+            assert.equal(res, expected);
+        });
+    });
+
+    describe('.removePrefix', () => {
+        it('removes a prefix from a string', () => {
+            assert.equal(util.removePrefix('Hello world', 'Hello '), 'world');
+        });
+    });
+
+    describe('.throwIfDefined', () => {
+        it('throws if the value is not undefined', () => {
+            assert.throws(() => util.throwIfDefined('something'));
+            assert.throws(() => util.throwIfDefined(null));
+            assert.throws(() => util.throwIfDefined(false));
+            assert.throws(() => util.throwIfDefined(0));
+        });
+        it('does not throw if the value is undefined', () => {
+            assert.doesNotThrow(() => util.throwIfDefined());
+            assert.doesNotThrow(() => util.throwIfDefined(undefined));
+        });
+    });
+
+    describe('.hasMethods', () => {
+        it('checks if an object has a set of methods', () => {
+            const obj = {
+                hello() {
+                },
+                world() {
+                }
+            };
+
+            assert.ok(util.hasMethods(obj, ['hello', 'world']));
+            assert.ok(! util.hasMethods(obj, ['hello', 'world', 'foo']));
+            assert.ok(! util.hasMethods(obj, ['bar']));
+        });
+    });
+
+    describe('.implementOrThrow', () => {
+        it('throws a TypeError if the object doesn\'t have the methods listed', () => {
+            const obj = {
+                hello() {
+                },
+                world() {
+                }
+            };
+
+            assert.doesNotThrow(() => util.implementOrThrow(obj, ['hello', 'world']));
+            assert.throws(() => util.implementOrThrow(obj, ['hello', 'world', 'baz']));
+        });
+    });
+
+    describe('.isDefined', () => {
+        it('returns whether the values is !== undefined', () => {
+            assert.ok(util.isDefined(''));
+            assert.ok(util.isDefined(0));
+            assert.ok(util.isDefined(false));
+            assert.ok(util.isDefined(null));
+
+            assert.equal(util.isDefined(), false);
+            assert.equal(util.isDefined(undefined), false);
+        });
+    });
+
+    describe('.removeSuffix', () => {
+        it('removes a suffix from a string', () => {
+            assert.equal(util.removeSuffix('hello world', ' world'), 'hello');
+        });
+    });
+    describe('.upperCaseFirst', () => {
+        it('uppercases the first letter of a word', () => {
+            assert.equal(util.upperCaseFirst('hello world'), 'Hello world');
+            assert.equal(util.upperCaseFirst('hELLO WORLD'), 'HELLO WORLD');
+            assert.equal(util.upperCaseFirst('Hello world'), 'Hello world');
+        });
+    });
+});
